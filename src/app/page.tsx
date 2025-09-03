748d24f9a300189d597d6573bba60de2465d358a
Namandeep Official <namandeepofficial@gmail.com>
2025-09-03 19:55:58 +0000

    in headline sections add featurs like Features , Reserch process ,Resour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import * as React from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
